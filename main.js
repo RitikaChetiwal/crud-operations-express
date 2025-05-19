96cc87786b8f4cefdf81f44ce75f3e753e2f36cb
Ritika Chetiwal <ritikachetiwal7>
2025-05-19 21:19:36 +0530

    posted users

---
+++ main.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const members = require('./members');
+const uuid = require('uuid');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hello crud!')
+});
+
+// GET REQUEST------------------------
+
+app.get("/getAllUsers", (req, res) => {
+    res.status(200).json(members);
+    res.json({ status: 'success', data: members });
+})
+
+app.get("/getUser/:uid", (req, res) => {
+    // console.log(typeof req.params.userId)
+    const userId = parseInt(req.params.uid);
+    let user = members.filter(member => member.id === userId);
+    console.log(user);    // user is an empty array,  
+    console.log(members);
+
+    user.length !== 0 ? res.status(200).json(user) : res.status(400).json({ message: `User not found with id ${userId}` });
+    // if (user.length !== 0) res.status(200).json(user) 
+    //     else res.status(404).json({message : `User not found with id ${id}`})
+});
+
+// POST REQUEST ------------------------------------------->
+app.post("/postUser", (req, res) => {
+    // console.log("User : ", req.body);
+    // const name = req.body.name;
+    // const email = req.body.email;
+
+    const { name, email } = req.body;
+    // console.log(name, email)
+
+    members.push({
+        id: uuid.v4(),
+        name,
+        email
+    })
+    res.status(200).json(members);
+
+});
+
+
+app.listen(port, () => {
+    console.log(`server is running at port:${port}`);
+    
+})
